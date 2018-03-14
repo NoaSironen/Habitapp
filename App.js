9eea54dae12578bd994f8371fab4b0faf78690a0
NoaSironen <noa.sironen@gmail.com>
2018-03-14 13:18:38 +0100

    Added drawermenu and stacknavigator

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { StyleSheet, TextInput, Platform, Image, Text, View, Navigator, TouchableOpacity } from 'react-native';
 import firebase from 'react-native-firebase';
-import {StackNavigator, DrawerNavigator} from 'react-navigation';
+import {StackNavigator, DrawerNavigator, NavigationAction} from 'react-navigation';
+import { Icon } from 'react-native-elements';
 import ChooseRegistration from './components/ChooseRegistration';
 import AddWorker from './components/AddWorker';
 import Home from './components/Home';
 
-const Navigation = DrawerNavigator({
-  Home: { screen: Home},
-  AddWorker: { screen: AddWorker},
-  ChooseRegistration: { screen: ChooseRegistration},
-}, {
-  // Default config for all screens
-  headerMode: 'float',
-  initialRouteName: 'Home', // The first screen to show up when the app starts, should be loging or home.
+const StackNavigation = StackNavigator ({
+  Home: { screen: Home },
+  AddWorker: { screen: AddWorker },
+  //ChooseRegistration: { ChooseRegistration },
+}, { 
+  headerMode: 'none'
 });
 
+const Navigation = DrawerNavigator({
+  Home: { 
+    screen: Home,
+    navigationOptions: {
+      title: 'Home', // Header title
+    }
+  },
+  AddWorker: { 
+    screen: AddWorker,
+    title: 'Add Worker',
+  },
+  ChooseRegistration: { 
+    screen: ChooseRegistration,
+    title: 'Choose Registraton',
+  },
+});
 
 export default Navigation;
