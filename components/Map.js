eda99b66dceb4ffbef069277d03a97319fde9f9f
Tobias Tunedal <tunedal.tobias@hotmail.com>
2018-04-03 13:29:34 +0200

    GPS but it says that i'm in San Francisco

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, StyleSheet, Image} from 'react-native';
+import {View, Text, TextInput, StyleSheet, Dimensions} from 'react-native';
 import {StackNavigator, NavigationAction} from 'react-navigation';
 import firebase from 'react-native-firebase';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
+const {width, height} = Dimensions.get('window')
+
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATID = width / height
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATITUDE_DELTA = ASPECT_RATID
 
 
 export default class Map extends Component {
@@ -12,13 +19,18 @@ constructor(props) {
 
   this.state = {
     initialPosition: {
-      latitude: 59.334591,
-      longitude: 18.063240,
-      latitudeDelta: 0.1,
-      longitudeDelta: 0.1,
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0,
+      longitudeDelta: 0
+    },
+    markerPosition : {
+      latitude: 0,
+      longitude: 0
     }
   }
 }
+
     mapStyle = [
   {
     "elementType": "geometry",
@@ -234,16 +246,60 @@ constructor(props) {
     ]
   }
 ]
+watchID: ?number = null // It is correct and it compiles, just a VS Code bug. 
+
+componentDidMount(){
+  navigator.geolocation.getCurrentPosition((position) => {
+    var lat = parseFloat(position.coords.latitude)
+    var long = parseFloat(position.coords.longitude)
+
+    var initialRegion = {
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA
+    }
+
+    this.setState({initialPosition: initialRegion})
+    this.setState({markerPosition: initialRegion})
+  }, 
+  (error) => alert(JSON.stringify(error)),
+  {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+  this.watchID = navigator.geolocation.watchPosition((position) => {
+    var lat = parseFloat(position.coords.latitude)
+    var long = parseFloat(position.coords.longitude)
+
+    var lastRegion = {
+      latitude: lat,
+      longitude: long,
+      longitudeDelta: LONGITUDE_DELTA,
+      latitudeDelta: LATITUDE_DELTA
+    }
+    this.setState({initialPosition: lastRegion})
+    this.setState({markerPosition: lastRegion})
+  })
+}
+
+componentWillUnmount() {
+  navigator.geolocation.clearWatch(this.watchID)
+}
       render() {
         return(
 
            <View style={styles.container}>
             <MapView style={styles.map}
                 customMapStyle={this.mapStyle}
-                initialRegion={this.state.initialPosition}     
-            >
+                region={this.state.initialPosition}>
+
+                <MapView.Marker 
+                coordinate={this.state.markerPosition}>
+                <View style={styles.radius}>
+                <View style={styles.marker}>
+                </View>
+                </View>
+                </MapView.Marker>
             </MapView>
-        
            </View>
         )
     }
@@ -268,10 +324,25 @@ const styles = StyleSheet.create({
         bottom: 0,
         right: 0
     },
-    picture: {
-        width: 400,
-        height:400,
-        opacity: 0.1,
+    radius: {
+        height: 50,
+        width: 50,
+        borderRadius: 50 / 2,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(0, 112, 255, 0.1)',
+        borderWidth: 1,
+        borderColor: 'rgba(0, 112, 255, 0.3)',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    marker: {
+      height: 20,
+      width: 20,
+      borderWidth: 3,
+      borderColor: 'white',
+      borderRadius: 20 / 2,
+      overflow: 'hidden',
+      backgroundColor: '#007AFF'
     },
     inputStyle: {
         height: 60,
