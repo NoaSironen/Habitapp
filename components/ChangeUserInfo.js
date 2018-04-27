cd91c2adc45be24b99b7e773ac16f84314230ad0
NoaSironen <noa.sironen@gmail.com>
2018-04-27 14:04:34 +0200

    Added on press to ProfilePicture screen

--- components/ChangeUserInfo.js
+++ components/ChangeUserInfo.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import { StyleSheet, TextInput, Platform, Image, Text, View, Navigator, TouchableOpacity, Alert, FlatList, KeyboardAvoidingView} from 'react-native';
-import {StackNavigator, NavigationAction} from 'react-navigation';
+import React, { Component } from 'react';
+import { StyleSheet, TextInput, Platform, Image, Text, View, Navigator, TouchableOpacity, Alert, FlatList, KeyboardAvoidingView } from 'react-native';
+import { StackNavigator, NavigationActions } from 'react-navigation';
 import firebase from 'react-native-firebase';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import ProfilePicture from './ProfilePicture'
@@ -9,169 +9,169 @@ const rootRef = firebase.database().ref();
 const userRef = rootRef.child('users');
 
 export default class ChangeUserInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          
-          /* userInfo : [], */
-          emailState : '',
-          firstNameState: '',
-          lastNameState: '',
-          phoneNumberState: '',
-          paymentCardNumberState: '',
-          profilePictureState: ''
-        };
-      } 
+  constructor(props) {
+    super(props);
+    this.state = {
 
-componentWillMount(){
-  var user = firebase.auth().currentUser;
-  var uid;
-  var dbEmail;
+      /* userInfo : [], */
+      emailState: '',
+      firstNameState: '',
+      lastNameState: '',
+      phoneNumberState: '',
+      paymentCardNumberState: '',
+      profilePictureState: ''
+    };
+  }
 
-  if (user != null) {
-    uid = user.uid;
-    dbEmail = user.email;
-  } 
+  componentWillMount() {
+    var user = firebase.auth().currentUser;
+    var uid;
+    var dbEmail;
 
-  var that = this;
-  let firebaseRef = firebase.database().ref().child('users/' + uid);
-  
- /*  var finished = []; */
+    if (user != null) {
+      uid = user.uid;
+      dbEmail = user.email;
+    }
 
-  firebaseRef.once('value', snapshot => {
-/*     let result = snapshot.val(); */
-    let dbFirstName = snapshot.child('firstName').val();
-    let dbLastName = snapshot.child('lastName').val();
-    let dbPhoneNumber = snapshot.child('phoneNumber').val();
-    let dbPaymentCardNumber = snapshot.child('paymentCardNumber').val();
-    let dbProfilePicture = snapshot.child('profilePicture').val();
-/*     result['key'] = snapshot.key; */
-    that.setState({
-      emailState: dbEmail,
-      firstNameState: dbFirstName,
-      lastNameState: dbLastName,
-      phoneNumberState: dbPhoneNumber, 
-      paymentCardNumberState: dbPaymentCardNumber,
-      profilePictureState: dbProfilePicture,
+    var that = this;
+    let firebaseRef = firebase.database().ref().child('users/' + uid);
+
+    /*  var finished = []; */
+
+    firebaseRef.once('value', snapshot => {
+      /*     let result = snapshot.val(); */
+      let dbFirstName = snapshot.child('firstName').val();
+      let dbLastName = snapshot.child('lastName').val();
+      let dbPhoneNumber = snapshot.child('phoneNumber').val();
+      let dbPaymentCardNumber = snapshot.child('paymentCardNumber').val();
+      let dbProfilePicture = snapshot.child('profilePicture').val();
+      /*     result['key'] = snapshot.key; */
+      that.setState({
+        emailState: dbEmail,
+        firstNameState: dbFirstName,
+        lastNameState: dbLastName,
+        phoneNumberState: dbPhoneNumber,
+        paymentCardNumberState: dbPaymentCardNumber,
+        profilePictureState: dbProfilePicture,
+      })
+
     })
+  }
+  updateUserContent = () => {
+    const { navigate } = this.props.navigation;
 
-  })
-}
-updateUserContent = () => {
-  const { navigate } = this.props.navigation;
+    var user = firebase.auth().currentUser;
+    var uid;
 
-  var user = firebase.auth().currentUser;
-  var uid;
-  
 
-  if (user != null) {
-    uid = user.uid;
+    if (user != null) {
+      uid = user.uid;
 
-  } 
-   userRef.child(uid).set({
-    firstName: this.state.firstNameState,
-    lastName: this.state.lastNameState,
-    phoneNumber: this.state.phoneNumberState,
-    paymentCardNumber: this.state.paymentCardNumberState,
-    profilePicture: this.state.profilePictureState
-  })
-  Alert.alert('Du har nu uppdaterat dina uppgifter!'); 
-  navigate('Home');
-  user.updateEmail(this.state.emailState).then(function() {
-    
-  }).catch(function(error) {
-  Alert.alert(
-    'Du måste logga in på nytt för att byta lösenord.',
-    'Vill du göra det?',
-    [
-      {text: 'Ja', onPress: () => navigate('LogInScreen')},
-      {text: 'Nej', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-    ],
-    { cancelable: true }
-  )
-});
-}  
+    }
+    userRef.child(uid).set({
+      firstName: this.state.firstNameState,
+      lastName: this.state.lastNameState,
+      phoneNumber: this.state.phoneNumberState,
+      paymentCardNumber: this.state.paymentCardNumberState,
+      profilePicture: this.state.profilePictureState
+    })
+    Alert.alert('Du har nu uppdaterat dina uppgifter!');
+    navigate('Home');
+    user.updateEmail(this.state.emailState).then(function () {
 
-    render() {
-      const { navigate } = this.props.navigation;
-      return(
+    }).catch(function (error) {
+      Alert.alert(
+        'Du måste logga in på nytt för att byta lösenord.',
+        'Vill du göra det?',
+        [
+          { text: 'Ja', onPress: () => navigate('LogInScreen') },
+          { text: 'Nej', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        ],
+        { cancelable: true }
+      )
+    });
+  }
 
-        <KeyboardAvoidingView behavior='padding' style={styles.container}>
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
 
-            
-              <View style={styles.logoContainer}>
-              <TouchableOpacity onPress={this.navigateToScreen('ChangeProfilePicture')}>
-              <Image style={styles.picture} source={require('../images/ProfileTemplate.png')} />
-              </TouchableOpacity>
-                {/* <Image style={styles.picture}  source={{uri: '.' + loggedInUser.profilePicture}}  /> */}
-              </View>
-            
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
 
-          
-                <View style={styles.linkLayout}> 
-                    <TouchableOpacity style={styles.linkStyle}>
-                        <Text style={styles.linkText}> BYT BANKKORT |</Text>
-                    </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.linkStyle} onPress={() => navigate('ChangeUserPassword')}>
-                        <Text style={styles.linkText}>| BYT LÖSENORD </Text>
-                    </TouchableOpacity> 
-                </View> 
+        <View style={styles.logoContainer}>
+          <TouchableOpacity onPress={() => navigate('ProfilePicture')}>
+            <Image style={styles.picture} source={require('../images/ProfileTemplate.png')} />
+          </TouchableOpacity>
+          {/* <Image style={styles.picture}  source={{uri: '.' + loggedInUser.profilePicture}}  /> */}
+        </View>
 
-{/*                 { this.state.userInfo.map(function(loggedInUser){
+
+
+        <View style={styles.linkLayout}>
+          <TouchableOpacity style={styles.linkStyle}>
+            <Text style={styles.linkText}> BYT BANKKORT |</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.linkStyle} onPress={() => navigate('ChangeUserPassword')}>
+            <Text style={styles.linkText}>| BYT LÖSENORD </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/*                 { this.state.userInfo.map(function(loggedInUser){
                    return( */}
-                    <TextInput style={styles.inputStyle} 
-                   /*  key={loggedInUser.key} */
-                    value={this.state.emailState}
-                    returnKeyType='next'
-                    keyboardType='email-address'
-                    autoCapitalize='none'
-                    onChangeText={
-                      (text) => {
-                        this.setState({ emailState: text });
-                      }
-                    }
-                  />
-{/*                   )
+        <TextInput style={styles.inputStyle}
+          /*  key={loggedInUser.key} */
+          value={this.state.emailState}
+          returnKeyType='next'
+          keyboardType='email-address'
+          autoCapitalize='none'
+          onChangeText={
+            (text) => {
+              this.setState({ emailState: text });
+            }
+          }
+        />
+        {/*                   )
                 })}  */}
-              
-                   <TextInput style={styles.inputStyle} 
-                   value={this.state.firstNameState}
-                    returnKeyType='next'
-                    keyboardType='default'
-                    onChangeText={
-                      (text) => {
-                        this.setState({ firstNameState : text });
-                      }
-                    }
-                  />
 
-                   <TextInput style={styles.inputStyle} 
-                   value={this.state.lastNameState}
-                    returnKeyType='next'
-                    keyboardType='default'
-                    onChangeText={
-                      (text) => {
-                        this.setState({ lastNameState: text });
-                      }
-                    }
-                  />
+        <TextInput style={styles.inputStyle}
+          value={this.state.firstNameState}
+          returnKeyType='next'
+          keyboardType='default'
+          onChangeText={
+            (text) => {
+              this.setState({ firstNameState: text });
+            }
+          }
+        />
 
-                    <TextInput style={styles.inputStyle} 
-                    value={this.state.phoneNumberState} 
-                    keyboardType='phone-pad'
-                    returnKeyType='next'
-                    onChangeText={
-                      (text) => {
-                        this.setState({ phoneNumberState: text });
-                      }
-                    }
-                  />
+        <TextInput style={styles.inputStyle}
+          value={this.state.lastNameState}
+          returnKeyType='next'
+          keyboardType='default'
+          onChangeText={
+            (text) => {
+              this.setState({ lastNameState: text });
+            }
+          }
+        />
 
-                    <TouchableOpacity style={styles.buttonStyle}  
-                        onPress={this.updateUserContent}>
-                        <Text style={styles.buttonTextStyle}> SPARA </Text>
-                    </TouchableOpacity>
+        <TextInput style={styles.inputStyle}
+          value={this.state.phoneNumberState}
+          keyboardType='phone-pad'
+          returnKeyType='next'
+          onChangeText={
+            (text) => {
+              this.setState({ phoneNumberState: text });
+            }
+          }
+        />
+
+        <TouchableOpacity style={styles.buttonStyle}
+          onPress={this.updateUserContent}>
+          <Text style={styles.buttonTextStyle}> SPARA </Text>
+        </TouchableOpacity>
 
       </KeyboardAvoidingView>
     )
@@ -179,7 +179,7 @@ updateUserContent = () => {
 
 }
 
-  
+
 const styles = StyleSheet.create({
 
   container: {
