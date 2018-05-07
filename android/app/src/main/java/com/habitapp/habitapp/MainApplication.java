7a76c7124cb8bac6e4dc75769ebb24afe1d360fc
NoaSironen <noa.sironen@gmail.com>
2018-05-07 09:19:56 +0200

    Added react-naitve-image-picker

--- android/app/src/main/java/com/habitapp/habitapp/MainApplication.java
+++ android/app/src/main/java/com/habitapp/habitapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.habitapp.habitapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 //import co.apptailor.googlesignin.RNGoogleSigninPackage;
 //import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new ImagePickerPackage(),
         
             //new RNGoogleSigninPackage(),
             //new VectorIconsPackage(),
