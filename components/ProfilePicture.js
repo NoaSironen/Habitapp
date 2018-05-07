bbf41ea71b75f69301610515ac6c104aa0128e3e
NoaSironen <noa.sironen@gmail.com>
2018-05-07 09:32:41 +0200

    Added ProfilePicture.js

--- components/ProfilePicture.js
+++ components/ProfilePicture.js
@@ -1,4 +1,27 @@
-import React, {Component} from 'react';
-import {Text, Image, View, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { Text, Image, View, TouchableOpacity, NativeModules, PropTypes } from 'react-native';
 import firebase from 'react-native-firebase';
 
+export default class ProfilePicture extends Component {
+
+    onPressProfilePicture() {
+        NativeModules.ImagePickerManager.showImagePicker({
+            title: 'Set Profile Picture',
+        }, (picture) => {
+            if (picture.data) {
+                this.setState({
+                    profilePicture: {
+                        uri: 'data:image/jpeg;base64,' + picture.data, isStatic: true
+                    }
+                });
+            }
+        }
+        )
+    }
+    render() {
+        return (
+            <View>
+            </View>
+        )
+    }
+}
