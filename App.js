68222735fa6443078c2e38fc0dc37815bfef07c3
Noa Sironen <noa.sironen@gmail.com>
2018-05-23 17:51:17 +0200

    asd

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React, {Component} from 'react';
 import { StyleSheet, TextInput, Platform, Image, Text, View, Navigator, TouchableOpacity } from 'react-native';
 import firebase from 'react-native-firebase';
-<<<<<<< HEAD
 import { DrawerMenu } from './components/Router';
-=======
-import { CustomDrawer } from './components/Router';
->>>>>>> 35e80c7c9361333b9b09993047919decbc36eed5
 
 export default class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
         <DrawerMenu/>
-=======
-        <CustomDrawer/>
->>>>>>> 35e80c7c9361333b9b09993047919decbc36eed5
     );
   }
 }
