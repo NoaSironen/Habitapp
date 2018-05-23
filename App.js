266878664757a1df2fa2be898e3b176fd24a7d0c
Noa Sironen <noa.sironen@gmail.com>
2018-05-23 15:38:57 +0200

    Changed name from CustomDrawer to Router since it is what it is, a stacked stacknavigator inside a DrawerNavigator.

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import { StyleSheet, TextInput, Platform, Image, Text, View, Navigator, TouchableOpacity } from 'react-native';
 import firebase from 'react-native-firebase';
-import { DrawerMenu } from './components/Router';
+import { Router } from './components/Router';
 
 export default class App extends Component {
   render() {
     return (
-        <DrawerMenu/>
+        <Router/>
     );
   }
 }
