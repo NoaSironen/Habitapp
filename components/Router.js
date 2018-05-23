35e80c7c9361333b9b09993047919decbc36eed5
NoaSironen <noa.sironen@gmail.com>
2018-05-23 16:11:54 +0200

    Changed back from Router to CustomDrawer

--- components/Router.js
+++ components/Router.js
@@ -81,7 +81,7 @@ const StackNav = StackNavigator({
   },
 });
 // DrawerNavigator, nested stacknavigator inside so that you can navigate through the drawer to other screens
-export const Router = DrawerNavigator({
+export const CustomDrawer = DrawerNavigator({
 
   Home: {
     screen: StackNav,
